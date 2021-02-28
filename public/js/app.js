35476c5c24ab8ef6bff288dafbcbf75b8f2e3ac6
Claire <claire.cy.t@gmail.com>
2021-02-28 12:49:57 -0500

    Change style

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,31 @@
 console.log('javascript file')
+
+const fetchData= (location) =>{
+  fetch('http://localhost:3000/weather?search='+location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+        messageTwo.textContent = ''
+      } else {
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forcastData
+      }
+  
+    })
+  })
+}
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+let messageOne = document.querySelector('#message-1')
+let messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = ''
+messageTwo.textContent = ''
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value
+  fetchData(location)
+})
--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
+require('dotenv').config();
+const geocode = require('./utils/geocode')
+const forcast = require('./utils/forcast')
 
 const app = express()
 //Define paths for Express config
@@ -20,19 +23,60 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather App',
-    name: 'Claire'
+    name: 'Chuyu Tong'
   })
 })
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About',
-    name: 'Claire'
+    name: 'Chuyu Tong'
   })
 })
 app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help',
-    name: 'Claire'
+    name: 'Chuyu Tong'
+  })
+})
+
+app.get('/weather', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+  const address = req.query.search
+
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error })
+    }
+
+    forcast(latitude, longitude, (error, forcastData) => {
+      if (error) {
+        return res.send({ error })
+      }
+      res.send({
+        location, forcastData,
+        address: req.query.search
+      })
+    })
+  })
+
+
+
+})
+
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+
+  res.send({
+    products: []
   })
 })
 
