575571d2e54c8b86751c1b5601a6b3662337e214
Claire <claire.cy.t@gmail.com>
2021-02-28 13:21:52 -0500

    fix bugs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
-require('dotenv').config();
 const geocode = require('./utils/geocode')
 const forcast = require('./utils/forcast')
 
