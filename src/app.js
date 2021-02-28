5c371bf203ea7cce41221e68c82157310b2fd4e5
Claire <claire.cy.t@gmail.com>
2021-02-28 13:31:35 -0500

    Add dotenv

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
+require('dotenv').config();
 const geocode = require('./utils/geocode')
 const forcast = require('./utils/forcast')
 
